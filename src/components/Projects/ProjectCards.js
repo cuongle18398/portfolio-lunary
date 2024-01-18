1266a34d4c905f8ec7b5ed31d36f4e9724a446f5
Cuong Le <cuongle18398@gmail.com>
2024-01-18 14:44:10 +0700

    "Update project list"

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -14,25 +14,11 @@ function ProjectCards(props) {
           {props.description}
         </Card.Text>
         <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
+          <CgWebsite /> &nbsp;
+          Read More
         </Button>
         {"\n"}
         {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
       </Card.Body>
     </Card>
   );
