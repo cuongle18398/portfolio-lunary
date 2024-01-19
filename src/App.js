1b783bbded9f66d39eb3ba686927c46bd29c8027
Cuong Le <cuongle18398@gmail.com>
2024-01-19 22:10:30 +0700

    "Update 19.1"

--- src/App.js
+++ src/App.js
@@ -30,21 +30,27 @@ function App() {
   );
 
   return (
-    <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <Preloader load={load} />
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
+          {!load &&
+            <>
+              <Navbar />
+              <ScrollToTop />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/project" element={<Projects />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+              <Footer />
+
+            </>}
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion"
+
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}>
-  </div>;
+
+  return (
+    <AnimatePresence>
+      {props.load && (
+        <motion.div id={props.load ? "preloader" : "preloader-none"}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+      )}
+    </AnimatePresence>
+  )
 }
 
 export default Pre;
