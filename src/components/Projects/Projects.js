37a0f0fd1eaddf7f531fd15da2e33c409243e9a1
Cuong Le <cuongle18398@gmail.com>
2024-01-22 13:01:13 +0700

    "Update Project Slide"

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { motion, AnimatePresence } from "framer-motion";
+import Slider from "react-slick";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
 import socialpost from "../../Assets/Projects/Project-Bg/social-post.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import illu from "../../Assets/Projects/Project-Bg/illu.png";
+import layout from "../../Assets/Projects/Project-Bg/layout.png";
+import logo from "../../Assets/Projects/Project-Bg/logo.png";
+import package_ from "../../Assets/Projects/Project-Bg/package.png";
+import stationery from "../../Assets/Projects/Project-Bg/stationery.png";
 
 function Projects() {
+  const settings = {
+    dots: true,
+    lazyLoad: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    initialSlide: 2
+  };
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -27,70 +39,43 @@ function Projects() {
             <p style={{ color: "white" }}>
               Here are a few projects I've worked on recently.
             </p>
-            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={socialpost}
-                  title="SOCIAL POST"
-                  description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-                  ghLink="https://github.com/soumyajit4419/Chatify"
-                  demoLink="https://chatify-49.web.app/"
-                />
-              </Col>
-
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={editor}
-                  title="ILLUSTRATION"
-                  description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-                  ghLink="https://github.com/soumyajit4419/Editor.io"
-                  demoLink="https://editor.soumya-jit.tech/"
-                />
-              </Col>
-
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={leaf}
-                  title="LOGO"
-                  description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-                  ghLink="https://github.com/soumyajit4419/Plant_AI"
-                  demoLink="https://plant49-ai.herokuapp.com/"
-                />
-              </Col>
-
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={suicide}
-                  title="STATIONERY"
-                  description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-                  ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-                />
-              </Col>
-
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={emotion}
-                  title="POSM"
-                  description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-                  ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-                />
-              </Col>
-
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={bitsOfCode}
-                  title="PACKAGE"
-                  description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-                  ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-                  demoLink="https://blogs.soumya-jit.tech/"
-                />
-              </Col>
-            </Row>
+            <br />
+            <br />
+            <Slider {...settings}>
+              <ProjectCard
+                imgPath={socialpost}
+                title="SOCIAL POST"
+              // ghLink="https://github.com/soumyajit4419/Chatify"
+              />
+              <ProjectCard
+                imgPath={illu}
+                title="ILLUSTRATION"
+              // ghLink="https://github.com/soumyajit4419/Editor.io"
+              />
+              <ProjectCard
+                imgPath={layout}
+                title="LAYOUT"
+              />
+              <ProjectCard
+                imgPath={logo}
+                title="LOGO"
+              />
+              <ProjectCard
+                imgPath={package_}
+                title="PACKAGE"
+              />
+              <ProjectCard
+                imgPath={stationery}
+                title="STATIONERY"
+              />
+            </Slider>
+            <br />
+            <br />
+            <br />
           </motion.div>
         </AnimatePresence>
       </Container>
-    </Container>
+    </Container >
   );
 }
 
