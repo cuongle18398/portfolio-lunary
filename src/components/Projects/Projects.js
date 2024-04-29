bddd816703d66088c6f63effe0f2c3d8d169c09f
Cuong Le <cuongle18398@gmail.com>
2024-04-29 20:33:00 +0700

    Update 29.4

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,21 +1,51 @@
 import React from "react";
+import { useState } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 import ProjectCard from "./ProjectCards";
 import socialpost from "../../Assets/Projects/Project-Bg/social-post.png";
 import illu from "../../Assets/Projects/Project-Bg/illu.png";
 import layout from "../../Assets/Projects/Project-Bg/layout.png";
 
 function Projects() {
+
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState('1');
+
+  const radios = [
+    { name: 'Digital', value: '1' },
+    { name: 'Event', value: '2' },
+    { name: 'Branding', value: '3' },
+    { name: 'More', value: '4' },
+  ];
   return (
 
     <Container fluid className="project-section" id="project">
       <Container>
         <h1 className="project-heading">
-          MY RECENT <strong className="purple">WORKS </strong>
+          MY <strong className="purple">PROJECTS </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Where my journey has begun.......
         </p>
+        <ButtonGroup>
+          {radios.map((radio, idx) => (
+            <ToggleButton
+              key={idx}
+              id={`radio-${idx}`}
+              type="radio"
+              variant={'outline-light'}
+              name="radio"
+              value={radio.value}
+              checked={radioValue === radio.value}
+              onChange={(e) => setRadioValue(e.currentTarget.value)}
+            >
+              {radio.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
+        <br />
         <br />
         <Row>
           <Col md={6} className="project-container">
